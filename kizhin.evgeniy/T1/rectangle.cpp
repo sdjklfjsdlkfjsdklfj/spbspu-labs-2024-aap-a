sdjklfjsdlkfjsdklfj/spbspu-labs

Initial commit for T1: implement essential clases

kizhin.evgeniy/T1/rectangle.cpp
@@ -1,0 +1,35 @@
+#include "rectangle.hpp"
+#include <cassert>
+
+kizhin::Rectangle::Rectangle(double width, double height, point_t position) :
+  data_ { width, height, position }
+{
+}
+
+double kizhin::Rectangle::getArea() const
+{
+  return data_.width * data_.height;
+}
+
+kizhin::rectangle_t kizhin::Rectangle::getFrameRect() const
+{
+  return data_;
+}
+
+void kizhin::Rectangle::move(double dx, double dy)
+{
+  data_.pos.x += dx;
+  data_.pos.y += dy;
+}
+
+void kizhin::Rectangle::move(const point_t& postition)
+{
+  data_.pos = postition;
+}
+
+void kizhin::Rectangle::scale(double scalingFactor)
+{
+  assert(scalingFactor > 0);
+  data_.height *= scalingFactor;
+  data_.width *= scalingFactor;
+}

sdjklfjsdlkfjsdklfj/spbspu-labs-2024-aap-a

kizhin.evgeniy/T1/rectangle.cpp
@@ -1,0 +1,35 @@
+#include "rectangle.hpp"
+#include <cassert>
+
+kizhin::Rectangle::Rectangle(double width, double height, point_t position) :
+  data_ { width, height, position }
+{
+}
+
+double kizhin::Rectangle::getArea() const
+{
+  return data_.width * data_.height;
+}
+
+const kizhin::rectangle_t& kizhin::Rectangle::getFrameRect() const
+{
+  return data_;
+}
+
+void kizhin::Rectangle::move(double dx, double dy)
+{
+  data_.pos.x += dx;
+  data_.pos.y += dy;
+}
+
+void kizhin::Rectangle::move(const point_t& postition)
+{
+  data_.pos = postition;
+}
+
+void kizhin::Rectangle::scale(double scaleFactor)
+{
+  assert(scaleFactor > 0);
+  data_.height *= scaleFactor;
+  data_.width *= scaleFactor;
+}